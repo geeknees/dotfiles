e35c95b5a4de4a7fe02194edc01b8767b02495ee
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2020-07-05 15:09:47 +0900

    Update

---
+++ tmux/base16.sh
@@ -0,0 +1,56 @@
+# Base16 Styling Guidelines:
+
+base00=default   # - Default
+base01='#151515' # - Lighter Background (Used for status bars)
+base02='#7D8000' # - Selection Background
+base03='#909090' # - Comments, Invisibles, Line Highlighting
+base04='#505050' # - Dark Foreground (Used for status bars)
+base05='#D0D0D0' # - Default Foreground, Caret, Delimiters, Operators
+base06='#E0E0E0' # - Light Foreground (Not often used)
+base07='#F5F5F5' # - Light Background (Not often used)
+base08='#AC4142' # - Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+base09='#D28445' # - Integers, Boolean, Constants, XML Attributes, Markup Link Url
+base0A='#F4BF75' # - Classes, Markup Bold, Search Text Background
+base0B='#90A959' # - Strings, Inherited Class, Markup Code, Diff Inserted
+base0C='#75B5AA' # - Support, Regular Expressions, Escape Characters, Markup Quotes
+base0D='#6A9FB5' # - Functions, Methods, Attribute IDs, Headings
+base0E='#AA759F' # - Keywords, Storage, Selector, Markup Italic, Diff Changed
+base0F='#8F5536' # - Deprecated, Opening/Closing Embedded Language Tags, e.g. <? php ?>
+
+set-option -g status-left-length 32
+set-option -g status-right-length 150
+set-option -g status-interval 5
+
+# default statusbar colors
+set-option -g status-style fg=$base02,bg=$base00,default
+
+# window colors
+set-option -g window-style bg=$base04
+set-window-option -g window-status-style fg=$base03,bg=$base00
+set-window-option -g window-status-format " #I #W"
+
+# active window title colors
+set-option -g window-active-style bg=$base01
+set-window-option -g window-status-current-style fg=$base0C,bg=$base00
+set-window-option -g window-status-current-format " #[bold]#W"
+
+# pane border colors
+set-window-option -g pane-active-border-style fg=$base0C
+set-window-option -g pane-border-style fg=$base03
+
+# message text
+set-option -g message-style fg=$base0C,bg=$base00
+
+# pane number display
+set-option -g display-panes-active-colour $base0C
+set-option -g display-panes-colour $base01
+
+# clock
+set-window-option -g clock-mode-colour $base0C
+
+tm_session_name="#[default,fg=$base0E,bg=$base00]#{?client_prefix,#[reverse],} #S "
+set-option -g status-left "$tm_session_name"
+
+tm_date="#[fg=$base00,bg=$base0C] %D %a %R"
+tm_host="#[fg=$base00,bg=$base0E] #h "
+set-option -g status-right "$tm_date $tm_host"
